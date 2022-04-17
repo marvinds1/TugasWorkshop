987aafd39fd81032f99e5628832fc7aeb865367c
A. Rifqi Afwan Muslihani <72347585+marvinds1@users.noreply.github.com>
2022-04-18 02:36:25 +0700

    Revert "update 2.1"

--- workshop1/app.py
+++ workshop1/app.py
@@ -1,4 +1,4 @@
-from flask import (Flask, render_template, request, redirect, session, url_for)
+from flask import (Flask, render_template, request, redirect, session, url_for) 
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -10,11 +10,9 @@ mysql = MySQL(app)
 
 app.secret_key = 'abcdefghijklmnopqrstuvwxyz'
 
-
 @app.before_first_request
 def awalan():
     session['loged'] = False
-
 
 @app.route('/')
 @app.route('/start')
@@ -24,8 +22,8 @@ def start():
 
 @app.route('/logout')
 def logout():
-    session.pop('username', None)
-    session.pop('iduser', None)
+    session.pop('username',None)
+    session.pop('iduser',None)
     session['loged'] = False
     return redirect('/login')
 
@@ -33,13 +31,12 @@ def logout():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-
+        
         username = request.form['username']
         password = request.form['password']
 
         cursor = mysql.connection.cursor()
-        cursor.execute(
-            "SELECT IdUser FROM user_accounts WHERE username=%s", (username,))
+        cursor.execute("SELECT IdUser FROM user_accounts WHERE username=%s",(username,))
         iduser = cursor.fetchall()
         if cursor.execute(' SELECT * FROM user_accounts WHERE username=%s or password=%s ', (username, password)):
             if cursor.execute(' SELECT * FROM user_accounts WHERE username=%s and password=%s ', (username, password)):
@@ -63,13 +60,12 @@ def home():
     if cond == True:
         iduser = session['iduser']
         cur = mysql.connection.cursor()
-        cur.execute(
-            "SELECT * FROM product_details ORDER BY dates DESC LIMIT 10")
+        cur.execute("SELECT * FROM product_details ORDER BY dates DESC LIMIT 10")
         rv = cur.fetchall()
-        cur.execute("SELECT * FROM user_accounts WHERE IdUser=%s", (iduser,))
+        cur.execute("SELECT * FROM user_accounts WHERE IdUser=%s",(iduser,))
         us = cur.fetchall()
         cur.close()
-        return render_template('home.html', data=rv, user=us)
+        return render_template('home.html', data=rv,user=us)
     else:
         return render_template('nicetry.html')
 
@@ -110,11 +106,10 @@ def forget_password():
 def details(id_data):
     cur = mysql.connection.cursor()
     cur.execute(
-        "SELECT * FROM product_details JOIN user_accounts ON product_details.IdUser=user_accounts.IdUser WHERE product_name=%s",
+        "SELECT * FROM product_details JOIN user_accounts ON product_details.IdUser=user_accounts.IdUser WHERE product_name=%s", 
         (id_data,))
     det = cur.fetchall()
     return render_template('details.html', det=det)
-
 
 @app.route('/detailprofile', methods=["GET"])
 def detailsprofile():
@@ -123,8 +118,7 @@ def detailsprofile():
     cur.execute(
         "SELECT * FROM user_accounts WHERE IdUser=%s", (iduser,))
     det = cur.fetchall()
-    return render_template('detailprofile.html', user=det)
-
+    return render_template('detailprofile.html',user=det)
 
 @app.route('/update', methods=["POST"])
 def update():
@@ -139,7 +133,6 @@ def update():
     mysql.connection.commit()
     return redirect(url_for('detailprofile'))
 
-
 @app.route('/updateitem', methods=["POST"])
 def updateitem():
     id_data = request.form['id']
@@ -150,10 +143,9 @@ def updateitem():
     link = request.form['foto']
     cur = mysql.connection.cursor()
     cur.execute("UPDATE product_details SET product_name=%s, price=%s, weight=%s, description=%s,photo=%s WHERE IdProduct=%s",
-                (nama, harga, berat, desc, link, id_data,))
+                (nama, harga, berat, desc,link, id_data,))
     mysql.connection.commit()
     return redirect(url_for('prodlist'))
-
 
 @app.route('/prodlist', methods=["GET"])
 def prodlist():
@@ -164,10 +156,9 @@ def prodlist():
     item = cur.fetchall()
     cur.execute("SELECT * FROM user_accounts WHERE IdUser=%s",
                 (iduser,))
-    user = cur.fetchall()
+    user=cur.fetchall()
     cur.close()
-    return render_template('productlist.html', item=item, user=user)
-
+    return render_template('productlist.html',item=item,user=user)
 
 @app.route('/hapus/<string:id_data>', methods=["GET"])
 def hapus(id_data):
@@ -176,8 +167,7 @@ def hapus(id_data):
     mysql.connection.commit()
     return redirect(url_for('prodlist'))
 
-
-@app.route('/simpan', methods=["POST"])
+@app.route('/simpan',methods=["POST"])
 def simpan():
     id_data = request.form['id']
     uname = request.form['uname']
@@ -188,21 +178,17 @@ def simpan():
     desc = request.form['deskripsi']
     cur = mysql.connection.cursor()
     cur.execute("INSERT INTO product_details VALUES (FLOOR(RAND()*(99999-10000+1))+10000,%s,%s,now(),%s,%s,%s,0,%s,%s,0)",
-                (id_data, uname, link, nama, harga, berat, desc,))
+                (id_data,uname,link,nama,harga,berat,desc,))
     mysql.connection.commit()
     return redirect(url_for('prodlist'))
-
 
 @app.route('/cart')
 def cart():
     iduser = session['iduser']
     cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM detail_cart RIGHT JOIN product_details ON detail_cart.IdProduct=product_details.IdProduct WHERE IdCart=(SELECT IdCart FROM cart WHERE IdUser = %s);", (iduser,))
-    cart = cur.fetchall()
-    cur.execute("SELECT SUM(amount * product_details.price) FROM detail_cart RIGHT JOIN product_details ON detail_cart.IdProduct=product_details.IdProduct WHERE IdCart=(SELECT IdCart FROM cart WHERE IdUser = %s);", (iduser,))
-    sum = cur.fetchall()
-    return render_template('cart.html', cart=cart, sum=sum)
-
+    cart=cur.fetchall()
+    return render_template('cart.html',cart=cart)
 
 @app.route('/addCart', methods=["POST"])
 def addCart():
@@ -210,32 +196,17 @@ def addCart():
     id_data = request.form['id']
     quantity = request.form['items']
     cur = mysql.connection.cursor()
-    if cur.execute("UPDATE product_details SET stock=stock-%s WHERE IdProduct = %s AND stock>=%s;", (quantity, id_data, quantity,)):
-        cur.execute("UPDATE product_details SET stock=stock-%s WHERE IdProduct = %s AND stock>=%s;",
-                    (quantity, id_data, quantity,))
-        cur.execute("INSERT INTO detail_cart VALUES (FLOOR(RAND()*(99999-10000+1))+10000, (SELECT IdCart FROM cart WHERE IdUser = %s), %s, %s,%s)",
-                    (iduser, iduser, id_data, quantity,))
-        mysql.connection.commit()
-        return redirect(url_for('cart'))
-    else:
-        cur.execute(
-            "SELECT * FROM product_details JOIN user_accounts ON product_details.IdUser=user_accounts.IdUser WHERE IdProduct=%s", (id_data,))
-        det = cur.fetchall()
-        return render_template('details.html', msg="Stock tidak mencukupi", det=det)
-
+    cur.execute("UPDATE product_details SET stock=stock-%s WHERE IdProduct = %s;", (quantity,id_data,))
+    cur.execute("INSERT INTO detail_cart VALUES (FLOOR(RAND()*(99999-10000+1))+10000, (SELECT IdCart FROM cart WHERE IdUser = %s), %s, %s,%s)", (iduser,iduser,id_data,quantity,))
+    mysql.connection.commit()
+    return redirect(url_for('home'))
 
 @app.route('/hapuscart/<string:id_data>', methods=["GET"])
 def hapuscart(id_data):
     cur = mysql.connection.cursor()
-    cur.execute(
-        "SELECT amount FROM detail_cart WHERE IdDetailCart=%s", (id_data,))
-    qt = cur.fetchall()
-    cur.execute(
-        "UPDATE product_details SET stock=stock+%s WHERE IdProduct = (SELECT IdProduct From detail_cart WHERE IdDetailCart=%s);", (qt, id_data,))
     cur.execute("DELETE FROM detail_cart WHERE IdDetailCart=%s", (id_data,))
     mysql.connection.commit()
     return redirect(url_for('cart'))
-
 
 if __name__ == '__main__':
     app.run(debug=True)
